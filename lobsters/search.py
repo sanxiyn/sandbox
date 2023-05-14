5aba9687d28a76c63907e3079c061bf3edeeb697
Seo Sanghyeon <sanxiyn@gmail.com>
2023-05-14 11:47:40 +0900

    Print count

--- lobsters/search.py
+++ lobsters/search.py
@@ -5,9 +5,12 @@ def search(topic):
     url = 'https://lobste.rs/search?what=stories&order=newest&q=' + topic
     response = requests.get(url)
     html = lxml.html.fromstring(response.text)
+    count = html.cssselect('h2')[0].text_content().strip()
+    count = ' '.join(count.split(' ')[:2])
+    print(count)
     for item in html.cssselect('.link a'):
         text = item.text_content()
-        print(text)
+        print(f' * {text}')
 
 import argparse
 parser = argparse.ArgumentParser()
